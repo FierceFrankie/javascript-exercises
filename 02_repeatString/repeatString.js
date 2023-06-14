8b3fdf8b18d7dc216717cc976827521926be0937
Frankietron <fiercefrankie@gmail.com>
2023-06-14 17:59:45 -0600

    finished first 3 exercises

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(stringInput, numTimes) {
+    let newOutput = ''
+    if (numTimes < 0) return 'ERROR'
+    for (let i = 0; i < numTimes; i++) {
+        newOutput += stringInput;
+    }
+    return newOutput;
 };
 
 // Do not edit below this line
