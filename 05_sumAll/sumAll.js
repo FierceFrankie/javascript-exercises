4d6a838d238f11cfa00d63e260bdd88c6af21ba2
Frankietron <fiercefrankie@gmail.com>
2023-06-15 00:55:05 -0600

    made num Range

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,8 +1,8 @@
-const sumAll = function(...nums) {
-    const total = nums.reduce((prevValue, curValue) => {
-        return prevValue + curValue;
-    }, 1)
-    return total;
+const sumAll = function(startNum, endNum) {
+    let numRange = [];
+    for (let i = startNum; i <= endNum; i++) {
+        numRange.push(i);
+    }
 };
 
 // Do not edit below this line
