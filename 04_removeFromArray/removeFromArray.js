1338c141cceebf8adb36271306d59c6e0fce0a37
Frankietron <fiercefrankie@gmail.com>
2023-06-14 23:39:21 -0600

    DID EXERCISE 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,10 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(numList, ...nums) {
+    for (let i = 0; i < nums.length; i++) {
+        if (numList.includes(nums[i])) {
+            numList.splice(numList.indexOf(nums[i]),1)
+        } 
+    }
+    return numList;
 };
 
 // Do not edit below this line
