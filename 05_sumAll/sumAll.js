55a92cb79ae1ab125b636f1ab95bf5a74205bad2
Frankietron <fiercefrankie@gmail.com>
2023-06-15 04:55:12 -0600

    update 05_sumAll/sumAll.js

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,11 +1,16 @@
 const sumAll = function(startNum, endNum) {
     if (typeof startNum != "number" || typeof endNum != "number" || startNum < 0 || endNum < 0) {
         return "ERROR";
-    } else {
+    } else if (startNum < endNum) {
         let numRange = [];
     for (let i = startNum; i <= endNum; i++) {
         numRange.push(i);
+    } else if (endNum < startNum) {
+        let numRange = [];
+    for (let i = startNum; i >= endNum; i--) {
+        numRange.push(i);
     }
+    } 
     const total = numRange.reduce((prevValue, curValue) => {
         return prevValue + curValue;
     }, 1)
