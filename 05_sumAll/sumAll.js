dbd7e168b193bdd83eaee7a2371c9d1815a2157d
Frankietron <fiercefrankie@gmail.com>
2023-06-15 18:09:06 -0600

    exercise 5 done

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,21 +1,23 @@
 const sumAll = function(startNum, endNum) {
+    let numRange = [];
     if (typeof startNum != "number" || typeof endNum != "number" || startNum < 0 || endNum < 0) {
-        return "ERROR";
-    } else if (startNum < endNum) {
-        let numRange = [];
-    for (let i = startNum; i <= endNum; i++) {
-        numRange.push(i);
-    } else if (endNum < startNum) {
-        let numRange = [];
-    for (let i = startNum; i >= endNum; i--) {
+        return 'ERROR';
+    } else if (startNum > endNum) {
+    for (let i = startNum; i > endNum; i--) {
         numRange.push(i);
     }
+   } else if (startNum < endNum) {
+    for (let i = endNum; i > startNum; i--) {
+        numRange.push(i);
     } 
-    const total = numRange.reduce((prevValue, curValue) => {
+} else if (startNum == endNum) {
+        numRange.push(startNum);
+    }
+   
+   const total = numRange.reduce((prevValue, curValue) => {
         return prevValue + curValue;
     }, 1)
     return total;
-    }
 };
 
 // Do not edit below this line
